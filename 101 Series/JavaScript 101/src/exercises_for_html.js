14ae0cf3f5c412ece0863486faa2a8263bd413bf
ReinhardBehrens <124669682+ReinhardBehrens@users.noreply.github.com>
2025-05-08 11:57:07 +0200

    Initial commit
    This is the initial commit for the Javascript exercises, this is a work in progress, and wil hopefully complete JavaScript in the next two to three weeks. Thereafter TypeScript, then React, and then React Native.

---
+++ 101 Series/JavaScript 101/src/exercises_for_html.js
@@ -0,0 +1,36 @@
+function clickme()
+{
+    console.log("Clicked!");
+}
+
+document.getElementById("clickme2").addEventListener("click", () => { 
+    console.log("Clicked! 2");
+});
+
+function changeTextForInput()
+{
+    document.getElementById("changeText").textContent = "Text Changed";
+}
+
+function changeBGColorViaQuerySelector()
+{
+    document.querySelector("div.boxClass").style.backgroundColor = "red";
+}
+
+let classBtn = document.getElementById("toggleBtn");
+classBtn.addEventListener("click", () => {
+    classBtn.classList.toggle("active");
+})
+
+function appendDocumentChild(child)
+{
+    let p = document.createElement(child);
+    p.textContent = "Dynamically added <p> tag";
+    document.body.appendChild(p);
+}
+
+function removeElementFromDOM(elementID)
+{
+    let el = document.getElementById(elementID);
+    el.remove();
+}
