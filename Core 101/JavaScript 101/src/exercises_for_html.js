0168e380d7f9c75262cff9785b36f418102a9a5f
ReinhardBehrens <reinhardforjesus@gmail.com>
2025-05-09 13:27:05 +0200

    Updated folder structure

---
+++ Core 101/JavaScript 101/src/exercises_for_html.js
@@ -0,0 +1,55 @@
+function clickme()
+{
+    console.log("Clicked!");
+}
+
+document.getElementById("clickme2").addEventListener("click", () => { 
+    console.log("Clicked! 2");
+});
+
+function changeTextForInput()
+{
+    document.getElementById("changeText").textContent = "Text Changed";
+}
+
+function changeBGColorViaQuerySelector()
+{
+    document.querySelector("div.boxClass").style.backgroundColor = "red";
+}
+
+let classBtn = document.getElementById("toggleBtn");
+classBtn.addEventListener("click", () => {
+    classBtn.classList.toggle("active");
+})
+
+function appendDocumentChild(child)
+{
+    let p = document.createElement(child);
+    p.textContent = "Dynamically added <p> tag";
+    document.body.appendChild(p);
+}
+
+function removeElementFromDOM(elementID)
+{
+    let el = document.getElementById(elementID);
+    el.remove();
+}
+
+/*
+    Exercise 50 - DOMContentLoaded Event (Memorize the structure used later during promises)
+*/
+function isDOMReady()
+{
+    return new Promise((resolve => {
+            if(document.readyState == "loading"){
+                document.addEventListener("DOMContentLoaded", () => resolve(true) );
+            }
+            else{
+                resolve = true;
+            }
+    }));
+}
+
+isDOMReady().then(()=> {
+    console.log("DOM Ready");
+});
